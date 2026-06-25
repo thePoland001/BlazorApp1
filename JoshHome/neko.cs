c75a0ca4456d735ab4610ac2ec434e949c6e0cd8
thepoland001 <jogi8758@colorado.edu>
2026-06-25 07:43:24 -0500

    refactored

---
+++ JoshHome/neko.cs
@@ -0,0 +1,17 @@
+namespace JoshHome{
+        public class NekosLifeResponse
+        {
+            public string? Url { get; set; }
+        }
+
+        public class NekosBestResponse
+        {
+            public NekosBestResult[]? Results { get; set; }
+        }
+
+        public class NekosBestResult
+        {
+            public string? Url { get; set; }
+        }
+    }
+ 
